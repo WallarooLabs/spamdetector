e8a9f05af2e79782c30f212a110c9f46799702ec
Simon Zelazny <zelazny@mailbox.org>
2018-06-15 08:07:12 -0700

    Launch EC2 instances with specific key and specific AMI

--- pulumi/bin/index.d.ts
+++
@@ -1,2 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-export declare let workerURL: pulumi.Output<string>;
--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,21 +1,46 @@
 // Copyright 2017, Pulumi Corporation.  All rights reserved.
+const aws = require("@pulumi/aws");
 
-import * as pulumi from "@pulumi/pulumi";
-import * as cloud from "@pulumi/cloud";
+let size = "t2.micro"
+let ami = "ami-0a521172"
 
-const sourcePort = 5555;
-
-let workers = new cloud.Service("ts-app-workers", {
-    containers: {
-        worker: {
-            build: "../spamdetector",
-            memory: 100,
-            ports: [{ port: sourcePort, external: true }]
-	}
-    },
-    replicas: 1
+let group = new aws.ec2.SecurityGroup("spamdetector-secgrp", {
+    ingress: [
+        { protocol: "tcp", fromPort: 22,
+	  toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+	{ protocol: "tcp", fromPort: 80,
+	  toPort: 80, cidrBlocks: ["0.0.0.0/0"] }
+    ],
 });
 
-export let workerURL = workers.endpoints.apply(e => e["worker"][sourcePort].hostname);
-// todo: get leader dns and connect a pool of workers
+let keyPair = new aws.ec2.KeyPair("spamdetector-key", {
+    publicKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvWfaxH0HFtmM6xypx9MqoswEpSVMlB7ZIgBcT1Wu+3PetcawbP+Mpzwi3ioT68srLVKBVq+VXOTfNJWKDqUvEnj5lrERGf8UlMj+uVXEPvXgVT0fyFUDxJGG2O5J0G1s1TO/nhxw0okR4OBN8PUm0LVPsSdDrSOXQvHD20zpu/HBq0X5X9YWmSwDqJ7aKntUIWRUw26U0df7WSPobnrNuVLYedvSi2ru5tXC8gpSvMoJalw5gwAR/sGRBacelnpT3faE5bdpsjQTxFD7gaaqwKwodPKjQm1AJKZHgW2Qh8wuQQ0JDGe5TpiM1t1/mukMYarKz0XnY15D5qEQ+iQ4r"})
 
+let startWallaroo =
+`#!/bin/sh
+nc -k -p 5556 -l 127.0.0.1 > sink.log &
+sudo python -m SimpleHTTPServer 80 &`;
+
+function newInstance(n: string) {
+    return new aws.ec2.Instance("spamdetector-" + n, {
+	instanceType: size,
+	securityGroups: [ group.name ],
+	ami: ami,
+	keyName: keyPair.keyName,
+	userData: startWallaroo
+    })
+}
+
+let chatServer = newInstance("chatServer")
+// let wallarooProcessors = ["w1","w2","w3"].map(newInstance);
+let wallarooProcessors = ["w1"].map(newInstance);
+
+
+function getInstanceInfo(i: any) {
+    return { "ip" : i.publicIp,
+	     "hostname" : i.publicDns
+	   }
+}
+
+exports.wallarooWorkers = wallarooProcessors.map(getInstanceInfo);
+exports.chatServer = getInstanceInfo(chatServer);
