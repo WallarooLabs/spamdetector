c7eb55d25656adb25b6d166e6a2f8b46e8aa6e0d
Simon Zelazny <zelazny@mailbox.org>
2018-06-18 10:50:31 -0700

    Make "constants" in stack description clearer

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,8 +1,10 @@
 import pulumi
 from pulumi_aws import ec2
 
-size = "t2.micro"
 ami = "ami-e308459b"
+instance_type = "t2.micro"
+wallaroo_instance_count = 1
+chat_instance_count = 1
 
 group = ec2.SecurityGroup("spamdetector-secgrp",
     ingress=[{ "protocol": "tcp", "fromPort": 22,
@@ -17,15 +19,12 @@ group = ec2.SecurityGroup("spamdetector-secgrp",
     ])
 
 key_pair = ec2.KeyPair("spamdetector-key",
-    public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvWfaxH0HFtmM6xypx9MqoswEpSVMlB7ZIgBcT1Wu+3PetcawbP+Mpzwi3ioT68srLVKBVq+VXOTfNJWKDqUvEnj5lrERGf8UlMj+uVXEPvXgVT0fyFUDxJGG2O5J0G1s1TO/nhxw0okR4OBN8PUm0LVPsSdDrSOXQvHD20zpu/HBq0X5X9YWmSwDqJ7aKntUIWRUw26U0df7WSPobnrNuVLYedvSi2ru5tXC8gpSvMoJalw5gwAR/sGRBacelnpT3faE5bdpsjQTxFD7gaaqwKwodPKjQm1AJKZHgW2Qh8wuQQ0JDGe5TpiM1t1/mukMYarKz0XnY15D5qEQ+iQ4r")
-
-wallaroo_instance_count = 1
-chat_instance_count = 1
+                       public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvWfaxH0HFtmM6xypx9MqoswEpSVMlB7ZIgBcT1Wu+3PetcawbP+Mpzwi3ioT68srLVKBVq+VXOTfNJWKDqUvEnj5lrERGf8UlMj+uVXEPvXgVT0fyFUDxJGG2O5J0G1s1TO/nhxw0okR4OBN8PUm0LVPsSdDrSOXQvHD20zpu/HBq0X5X9YWmSwDqJ7aKntUIWRUw26U0df7WSPobnrNuVLYedvSi2ru5tXC8gpSvMoJalw5gwAR/sGRBacelnpT3faE5bdpsjQTxFD7gaaqwKwodPKjQm1AJKZHgW2Qh8wuQQ0JDGe5TpiM1t1/mukMYarKz0XnY15D5qEQ+iQ4r")
 
 wallaroo_instances = []
 for i in range(0,wallaroo_instance_count):
     wallaroo_instances.append(ec2.Instance("spamdetector-%s"%(i+1,),
-                                           instance_type=size,
+                                           instance_type=instance_type,
                                            security_groups=[group.name],
                                            ami=ami,
                                            key_name=key_pair.key_name))
@@ -33,12 +32,10 @@ chat_instance_count = 1
 chat_instances = []
 for i in range(0,chat_instance_count):
     chat_instances.append(ec2.Instance("chatserver-%s"%(i+1,),
-                                       instance_type=size,
+                                       instance_type=instance_type,
                                        security_groups=[group.name],
                                        ami=ami,
                                        key_name=key_pair.key_name))
 
 pulumi.output('chat_hostnames', [ s.public_dns for s in chat_instances ])
 pulumi.output('wallaroo_hostnames', [ s.public_dns for s in wallaroo_instances ])
-
-
--- pulumi/generate_inventory.sh
+++ pulumi/generate_inventory.sh
@@ -1,7 +1,8 @@
 #1/bin/sh
 STACK=$(pulumi stack output | tail -n+3)
 CHAT_SERVER=$(echo "$STACK" | grep chat \
-		  | awk '{print $2}' | jq '.[]' | head -n1 \
+		  | awk '{print $2}' | jq '.[]' \
+		  | head -n1 \
 		  | xargs -n1 echo)
 WALLAROO_SERVERS=$(echo "$STACK" | grep wallaroo \
 		       | awk '{print $2}' | jq '.[]' \
@@ -12,4 +13,4 @@ for s in "$CHAT_SERVER"; do echo "$s ansible_user=ubuntu"; done
 
 
 echo "[wallaroo_workers]"
-for s in $WALLAROO_SERVERS; do echo "$s ansible_user=ubuntu"; done
+for s in "$WALLAROO_SERVERS"; do echo "$s ansible_user=ubuntu"; done
